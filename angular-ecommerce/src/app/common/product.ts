ee6645b1d98c4e39c11e18e87c34b4c34f2cb976
stephenmyers <spmyers26@gmail.com>
2024-03-30 12:05:23 -0500

    corrected spelling of unitsInStock property of Product class

--- angular-ecommerce/src/app/common/product.ts
+++ angular-ecommerce/src/app/common/product.ts
@@ -6,7 +6,7 @@ export class Product {
                 public unitPrice: number,
                 public imageUrl: string,
                 public active: boolean,
-                public UnitsInStock: number,
+                public unitsInStock: number,
                 public dateCreated: Date,
                 public lastUpdated: Date
     ) {
